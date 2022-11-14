40d6bdfdb91540eda33de32c029cfbb14dcba887
addliam <liamyostin2004@gmail.com>
2022-11-14 16:52:15 -0500

    initial setup for tailwindcss

---
+++ components/general/TopBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TopBar = () => {
+  return (
+    <div className='bg-primary'>
+        TopBar
+    </div>
+  )
+}
+
+export default TopBar
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0D6EFD",
+        secondary: "#6C757D",
+        success: "#198754",
+        danger: "#DC3545",
+        warning: "#FFC107",
+        info: "#0DCAF0",
+        white1: "#F8F9FA",
+        dark: "#212529",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+}
