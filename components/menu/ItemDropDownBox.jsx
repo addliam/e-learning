68dfdc0b99423a49b215790c9578694319656abc
addliam <liamyostin2004@gmail.com>
2022-11-15 16:26:06 -0500

    item dropdown box with styling

--- components/menu/ItemDropDownBox.jsx
+++ components/menu/ItemDropDownBox.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
 import Image from 'next/image'
+// @status must be in (complete, actual, locked)
+const ItemCapitulo = ({title, type, status}) => {
+    const itemClickHandler = () => {
+        console.log('Item clicked');
+    }
 
-const ItemCapitulo = ({title, type}) => {
+    const iconSwitch = (value) => {
+        switch (value) {
+            case "complete":
+                return (<Image src={"/assets/icons/okIcon.png"} width={22} height={22} alt="beginner" ></Image>)
+                break;
+            case "actual":
+                return (<Image src={"/assets/icons/nextBlueIcon.png"} width={22} height={22} alt="beginner" ></Image>)
+                break;
+            case "locked":
+                return (<Image src={"/assets/icons/lockIcon.png"} width={22} height={22} alt="beginner" ></Image>)
+                break;
+            default:
+                break;
+        }
+    }
     return (
-        <div className='bg-[#EAF0F3] w-[236px] h-[86px] rounded-md flex-shrink-0 py-[12px] px-[12px] relative'>
-            <h6 className='text-[#3A414B] text-[17px] font-semibold'>{title}</h6>
-            <span className='text-[13px] text-[#676767] font-semibold'>{type}</span>
+        <div className={`${status==='locked'?'':'cursor-pointer'} bg-[#EAF0F3] w-[232px] h-[86px] rounded-md flex-shrink-0 py-[12px] px-[12px] relative`} onClick={()=>itemClickHandler()}>
+            <h6 className={`text-[17px] font-semibold ${status==='locked'?'text-[#687281]':'text-[#3A414B]'}`} >{title}</h6>
+            <span className={`text-[13px] font-semibold ${status==='locked'?'text-[#989494]':'text-[#676767]'}`}>{type}</span>
             <div className='absolute right-3 bottom-3 '>
-                <Image src={"/assets/icons/okIcon.png"} width={22} height={22} alt="beginner" ></Image>              
+                {
+                    iconSwitch(status)
+                }
             </div>
         </div>
     )
@@ -15,11 +36,11 @@ const ItemCapitulo = ({title, type}) => {
 
 const ItemContent = () => {
     return (
-        <div className='w-[546px] bg-white flex flex-row flex-wrap px-[1.75rem] gap-[18px] pt-2 pb-4'>
-            <ItemCapitulo title={'1.1 Empezamos'} type={'Leccion'} />
-            <ItemCapitulo title={'1.2 Empezamos'} type={'Leccion'} />
-            <ItemCapitulo title={'1.3 Empezamos'} type={'Leccion'} />
-            <ItemCapitulo title={'1.4 Empezamos'} type={'Leccion'} />
+        <div className='max-w-[522px] bg-white flex flex-row flex-wrap px-[1.25rem] gap-[18px] pt-2 pb-4'>
+            <ItemCapitulo title={'1.1 Empezamos'} type={'Leccion'} status="complete"/>
+            <ItemCapitulo title={'1.2 Empezamos'} type={'Leccion'} status="complete"/>
+            <ItemCapitulo title={'1.3 Empezamos'} type={'Leccion'} status="actual"/>
+            <ItemCapitulo title={'1.4 Empezamos'} type={'Practica'} status="locked" />
         </div>
     )
 }
@@ -27,7 +48,7 @@ const ItemContent = () => {
 const ItemDropDownBox = () => {
   return (
     <>
-        <div className='w-[546px] h-[62px] bg-white flex flex-row gap-5 items-center  '>
+        <div className='max-w-[522px] h-[62px] bg-white flex flex-row gap-5 items-center  '>
             <div className='ml-[20px]'>
                 <Image src={"/assets/icons/introIcon.png"} width={42} height={42} alt="beginner" ></Image>
             </div>
--- pages/mruv.js
+++ pages/mruv.js
@@ -15,14 +15,14 @@ const mruv = () => {
       </section>
       <section className='flex flex-row gap-5 items-center justify-center'>
         <div className='img-wrap rounded-[50%] overflow-hidden '>
-          <Image style={'object-fit: "cover"'} src={"/assets/icons/carLogo.png"} width={82} height={82} alt="car" ></Image>
+          <Image src={"/assets/icons/carLogo.png"} width={82} height={82} alt="car" ></Image>
         </div>
         <div>
           <h3 className='font-semibold text-[26px]'>MRUV - Movimiento Rectilineo Uniformemente Variado</h3>
           <p className='max-w-[880px] text-[14px]'>El movil se desplaza sobre una trayectoria recta estando sometida a una aceleracion constante. Su velocidad varia constantemente, la velocidad aumenta o disminuye de forma permanente.</p>
         </div>
       </section>
-      <section>
+      <section className='mx-28 mt-12'>
         <ItemDropDownBox />
       </section>
     </main>
