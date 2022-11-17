3052ad06b51d7d65d9a67c3e35b38b2eb9cd716c
addliam <liamyostin2004@gmail.com>
2022-11-16 19:38:57 -0500

    dinamico detalle de contenido

--- pages/mruv/[slug].js
+++ pages/mruv/[slug].js
@@ -1,14 +1,21 @@
 import React from 'react'
 import SmallTopBar from '../../components/general/SmallTopBar'
 import { useRouter } from 'next/router'
+import content from '../../public/assets/content.json'
 
 const MruvId = () => {
     const router = useRouter()
     const { slug } = router.query
+    const itemsContent = content[0].items
+    const primerSubindice = itemsContent.find((item)=>item.subindice === 1.1)
+    console.log(primerSubindice);
     return (
         <>
             <SmallTopBar />
             {slug}
+            <p>El contenido del subindice {primerSubindice.subindice} es:</p>
+            <p>Numero de paginas: {primerSubindice.numeroPaginas}</p>
+            {primerSubindice.paginas[0].contenido}
         </>
     )
 }
